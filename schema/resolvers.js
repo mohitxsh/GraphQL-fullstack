7cf21f109f3908b17d4cdf9b732c20ca2b6ea7b4
mohitxsh <mohitxsh@gmail.com>
2022-07-07 11:45:45 +0530

    graphql-api

---
+++ schema/resolvers.js
@@ -0,0 +1,60 @@
+const { UserList, MovieList } = require("../FakeData");
+const _ = require("lodash");
+
+const resolvers = {
+  Query: {
+    users: () => {
+      return UserList;
+    },
+    user: (parent, args) => {
+      const id = args.id;
+      const user = _.find(UserList, { id: Number(id) });
+      return user;
+    },
+    movies: () => {
+      return MovieList;
+    },
+    movie: (parent, args) => {
+      const title = args.name;
+      const movie = _.find(MovieList, { name: title });
+      return movie;
+    },
+  },
+  User: {
+    favouriteMovies: () => {
+      return _.filter(
+        MovieList,
+        (movie) =>
+          movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010
+      );
+    },
+  },
+  Mutation: {
+    createUser: (parent, args) => {
+      const user = args.input;
+      const lastId = UserList[UserList.length - 1].id;
+      user.id = lastId + 1;
+      UserList.push(user);
+      return user;
+    },
+
+    updateUsername: (parent, args) => {
+      const { id, newUsername } = args.input;
+      let userUpdated;
+      UserList.forEach((user) => {
+        if (user.id === Number(id)) {
+          user.username = newUsername;
+          userUpdated = user;
+        }
+      });
+      return userUpdated;
+    },
+    deleteUser: (parent, args) => {
+      const id = args.id;
+      _.remove(UserList, (user) => user.id === Number(id));
+      return null;
+    },
+  },
+};
+
+module.exports = { resolvers };
